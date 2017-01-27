f8ca3ea2f7a2e3ee861a1919c2e896edbb99c83c
Pavlo Kapyshin <i@93z.org>
2017-01-27 13:29:07 +0100

    Simplify test loading

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,15 +1,9 @@
-import os
 import doctest
 import unittest
 
 
-# Most recent version is used anyway.
-README_PATH = os.path.join(os.path.dirname(__file__), "..", "README.rst")
-
-
 def load_tests(loader, tests, pattern):
     suite = doctest.DocFileSuite(
-        README_PATH, module_relative=False,
-        optionflags=doctest.NORMALIZE_WHITESPACE)
+        "../README.rst", optionflags=doctest.NORMALIZE_WHITESPACE)
     tests.addTests(suite)
     return tests
